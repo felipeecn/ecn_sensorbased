92b0b00452b46bb5e38e5ef96433c7c5b2da9e9a
felipeecn <felipe.silva-rangel-dos-santos@eleves.ec-nantes.fr>
2019-03-28 18:45:16 +0100

    quadratique

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,16 @@ int main(int argc, char** argv)
 
     // QP solver
     vpQuadProg qp;
+    vpMatrix Q, C(8,4), A(1,4);
+    Q.eye(4);
+    Q[2][2] = Q[3][3] = 0;
+    A[0][2] = A[0][3] = 0;
+
+    vpMatrix J2D(2,4), Jinv_robot(2,2);
+
+    vpColVector r(4, 0.0), b(1, 0.0), s(2,0.0), d(8,0.0), camLim(2,0.0);
+
+    vpFeaturePoint p;
 
 
     while(ros::ok())
@@ -41,7 +51,42 @@ int main(int argc, char** argv)
 
             cout << "v: " << v.t() << endl;
 
+            //Definition r as in Qx=r
+            r[0]    = v[0];
+            r[1]    = v[1];
+            //Definition of A as in Ax=b
+            A[0][0] = v[1];//w*
+            A[0][1] =-v[0];//-v*
+
+            //Construcion Q
+            robot.getImagePoint(s);
+            p.buildFrom(s[0],s[1],1);
+            J2D = p.interaction()*robot.getCamJacobian();
+
+            Jinv_robot[0][0] = Jinv_robot[1][0] = 1/robot.radius();
+            Jinv_robot[0][1] = robot.base()/robot.radius();
+            Jinv_robot[1][1] = - Jinv_robot[0][1];
+
+            ecn::putAt(C,J2D,0,0);
+            ecn::putAt(C,J2D,2,0);
+            ecn::putAt(C,Jinv_robot,4,0);
+            ecn::putAt(C,-Jinv_robot,6,0);
+
+            //Construction d
+            camLim = robot.getCamLimits();
+            ecn::putAt(d,(lc*(camLim-s)),0);
+            ecn::putAt(d,(lc*(s+camLim)),2);
+            for(int i=0;i<4;i++)
+                d[i+3] = robot.wmax();
+            //Solving system
+            qp.solveQP(Q,r,A,b,C,d,v);
+            //Sending setpoint
+            cout << "v: " << v.t() << endl;
             robot.setVelocity(v);
+
+            robot.radius();
+            robot.base();
+            robot.wmax();
         }
     }
 }
